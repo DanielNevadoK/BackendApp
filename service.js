856c363f0c28c30cbe61604218764dae317f81a8
DanielNevadoK <113299772+DanielNevadoK@users.noreply.github.com>
2023-09-20 11:15:05 +0200

    wont work

--- service.js
+++ service.js
@@ -1,11 +1,16 @@
-require('dotenv').config();
-const app = require('./server.js');
-var path = require('path');
-var favicon = require('serve-favicon');
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// Other middleware and route handlers can go here.
 
-app.listen(PORT, () => {
-    console.log(`http server listening on port ${PORT}`)
+// Use the favicon middleware to serve the favicon.ico file.
+const favicon = require('serve-favicon');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Start your server
+const port = 3000; // or any other port you prefer
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
+
