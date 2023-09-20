b55e8b9cfe947e59c8baebacfe492c3b3fdbb7c7
DanielNevadoK <113299772+DanielNevadoK@users.noreply.github.com>
2023-09-20 11:12:35 +0200

    installed favicon

--- service.js
+++ service.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const app = require('./server.js');
 var path = require('path');
+var favicon = require('serve-favicon');
 
 const PORT = process.env.PORT || 3000
-app.use(path.join(BackendApp, 'public'))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.listen(PORT, () => {
     console.log(`http server listening on port ${PORT}`)
